3248c32426207a65ea9a9c5197eda092822f196d
Pedro Ayres <pedrocgb21@gmail.com>
2022-08-15 23:42:26 -0300

    meu primeiro commit

---
+++ homepage.js
@@ -0,0 +1,11 @@
+const form = document.getElementsByClassName("faixa_login");
+const input = document.getElementsByClassName("faça_login");
+const [nome,senha,button] = input;
+
+function HandleSubmit (event) {
+    event.preventDefault();
+    window.location = "page2.html"
+}
+
+
+form.addEventListener("submit",HandleSubmit)
---
+++ page.js
@@ -0,0 +1,5 @@
+const name = "gustavo"
+
+function load(){
+    window.alert("hello " + name)
+}

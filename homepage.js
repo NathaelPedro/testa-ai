78707d8e17dcd51665285dda03e52d76fba6e8a0
Pedro Ayres <pedrocgb21@gmail.com>
2022-08-16 21:47:27 -0300

    teste 5

--- homepage.js
+++ homepage.js
@@ -1,6 +1,7 @@
 const form = document.getElementsByClassName("faixa_login");
 const input = document.getElementsByClassName("faça_login ");
 const [nome,senha,button] = input;
+console.log(window.location.href) 
 
 function HandleSubmit (event) {
     event.preventDefault();

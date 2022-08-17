2038b65b82f358e06c716ab5b0bb712d8cd68c23
Pedro Ayres <pedrocgb21@gmail.com>
2022-08-16 23:04:49 -0300

    sadas

--- js script pages/index.js
+++ js script pages/index.js
@@ -1,11 +1,11 @@
 const form = document.getElementsByClassName("faixa_login");
-const input = document.getElementsByClassName("faça_login ");
+const input = document.getElementsByClassName("faça_login");
 const [nome,senha,button] = input;
 console.log(window.location.href) 
 
 function HandleSubmit (event) {
     event.preventDefault();
-    window.location.href = "index2.html"
+    window.location = "/page/2.html"
 }
 
 
